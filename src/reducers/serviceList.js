69808eefb0b8def51faf5405d5d3b7e3feeb77f6
Dmitriy <chekanov.d@gmail.com>
2019-11-11 16:33:05 +0300

    first step

---
+++ src/reducers/serviceList.js
@@ -0,0 +1,50 @@
+import {
+  FETCH_SERVICES_REQUEST,
+  FETCH_SERVICES_FAILURE,
+  FETCH_SERVICES_SUCCESS,
+  REMOVE_SERVICE_REQUEST,
+  REMOVE_SERVICE_FAILTURE,
+  REMOVE_SERVICE_SUCCESS,
+} from '../actions/actionTypes'
+
+const initialState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+
+export default function serviceListReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SERVICES_REQUEST:
+    case REMOVE_SERVICE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_SERVICES_FAILURE:
+    case REMOVE_SERVICE_FAILTURE:
+      const {error} = action.payload;
+      return {
+        ...state,
+        loading: false,
+        error,
+      };
+    case FETCH_SERVICES_SUCCESS:
+      const {items} = action.payload;
+      return {
+        ...state,
+        items,
+        loading: false,
+        error: null,
+      };
+    case REMOVE_SERVICE_SUCCESS:
+      const {id} = action.payload;
+      return {
+        ...state,
+        items: state.items.filter(o => o.id !== id)
+      };
+    default:
+      return state;
+  }
+}
